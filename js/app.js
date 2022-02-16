154f3687e23eed12c202570cf3e7d48572320819
rahidhaque <ontirahid@yahoo.com>
2022-02-17 02:24:22 +0600

    Done until Expense & Balance

--- js/app.js
+++ js/app.js
@@ -1,19 +1,57 @@
 function getInputValue(inputId) {
-    //get finance
     const inputFinance = document.getElementById(inputId);
     const financeText = inputFinance.value;
-    const financeAmount = parseInt(financeText);
-    //clear input field
-    inputFinance.value = "";
-    return financeAmount;
+    if (isNaN(financeText)) {
+        alert("Please Correct!! You Have entered a string value");
+        return false;
+    }
+    else if (financeText == "") {
+        alert("Please Correct!! Some of your field is empty");
+        return false;
+    }
+    else {
+        //get finance
+        const financeAmount = parseFloat(financeText);
+        if (financeAmount < 0) {
+            alert("You have entered a negative number");
+        }
+        //clear input field
+        inputFinance.value = "";
+        return financeAmount;
+    }
 }
-
-
-document.getElementById('btn-calculate').addEventListener('click', function () {
+function updateFinanceField(totalFieldId, totalFinance) {
+    const financeField = document.getElementById(totalFieldId);
+    if (totalFinance > 0) {
+        financeField.innerText = totalFinance;
+    }
+}
+function getTotalIncome() {
     //get income
     const totalIncome = getInputValue('input-income');
+    return totalIncome;
+}
+
+function getTotalExpenses() {
     //get expenses
     const totalExpenses = getInputValue('input-food') + getInputValue('input-rent') + getInputValue('input-cloth');
-    const expensField = document.getElementById('expense-amount');
-    expensField.innerText = totalExpenses;
+    return totalExpenses;
+}
+
+/* function getTotalBalance(totalIncome, totalExpenses) {
+    const totalBalance = totalIncome - totalExpenses;
+    return totalBalance;
+} */
+
+document.getElementById('btn-calculate').addEventListener('click', function (event) {
+    const totalExpenses = getTotalExpenses();
+    const totalIncome = getTotalIncome();
+    const totalBalance = totalIncome - totalExpenses;
+    if (totalIncome < totalExpenses) {
+        alert("Your Total Income is greater than expense");
+    }
+    else {
+        updateFinanceField('expense-amount', totalExpenses);
+        updateFinanceField('balance-expense-amount', totalBalance);
+    }
 })
