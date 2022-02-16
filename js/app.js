6221b67f05a6c092ab831c951e0042fee869b650
rahidhaque <ontirahid@yahoo.com>
2022-02-16 23:37:25 +0600

    Input Function Written

--- js/app.js
+++ js/app.js
@@ -1,3 +1,17 @@
+function getInputValue(inputId) {
+    //get finance
+    const inputFinance = document.getElementById(inputId);
+    const financeText = inputFinance.value;
+    const financeAmount = parseInt(financeText);
+    //clear input field
+    inputFinance.value = "";
+    return financeAmount;
+}
+
+
 document.getElementById('btn-calculate').addEventListener('click', function () {
+    //get income
+    const incomeAmount = getInputValue('input-income');
+    //get expenditures
 
 })
